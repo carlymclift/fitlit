91412b0556f06e2c393e630551cb6a5bd0589d2a
Becca Steinbrecher <beccasteinbrecher@gmail.com>
2020-05-12 16:57:18 -0600

    pseudo code DOM

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+//fn to choose user at random
+
+//DOM -- info card displays all user's info
+
+//change welcome message --> welcome user's name
+
+//step goal - display how current user's step goal
+//compares to avg step goal for all users
