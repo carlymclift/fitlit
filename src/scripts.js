de5672beefbd618595c399e193763726fd8441a2
Carly Clift <carlymclift@gmail.com>
2020-05-12 22:31:57 -0600

    Add innerText to changeWelcome function so that it displays dynamic data

--- src/scripts.js
+++ src/scripts.js
@@ -2,8 +2,11 @@ let welcomeTitle = document.querySelector('.welcome');
 let userCardName = document.querySelector('.name');
 let userAddress = document.querySelector('.address');
 let userEmail = document.querySelector('.email');
+let userStepGoal = document.querySelector('.user-step-goal');
+let averageStepGoal = document.querySelector('.average-step-goal');
 
 let userArray = [];
+
 
 userData.forEach(user => {
 	let currentUser = new User(user);
@@ -11,7 +14,9 @@ userData.forEach(user => {
 })
 
 const currentUserData = new UserRepository(userArray);
+let userRepo = new UserRepository(userArray);
 let randomUser = {};
+
 
 let chooseRandom = () => {
 	let randomNum = (Math.floor(Math.random() * currentUserData.data.length));
@@ -24,9 +29,8 @@ let changeWelcome = () => {
 	userCardName.innerText = `Name: ${randomUser.name}`;
 	userAddress.innerText = `Address: ${randomUser.address}`;
 	userEmail.innerText = `Email: ${randomUser.email}`;
+	userStepGoal.innerText = `Your Step Goal: ${randomUser.dailyStepGoal}`;
+	averageStepGoal.innerText = `Average Step Goal ${userRepo.fetchAverageStepGoal()}`
 }
 
-
 window.addEventListener('load', changeWelcome);
-
-//step goal - display how current user's step goal compares to avg step goal for all users
