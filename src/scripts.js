5b181fd90561103fb9ea9e85418f04c86365fb58
Becca Steinbrecher <beccasteinbrecher@gmail.com>
2020-05-12 19:10:53 -0600

    feature/randomUser+greeting_onLoad

--- src/scripts.js
+++ src/scripts.js
@@ -1,18 +1,17 @@
-// const UserRepository = require('./UserRepository');
-// const userData = require('../data/users');
-
 let welcomeTitle = document.querySelector('.welcome');
+let userCardName = document.querySelector('.name');
+let userAddress = document.querySelector('.address');
+let userEmail = document.querySelector('.email');
 
 let userArray = [];
 
 userData.forEach(user => {
-	let currentUser = new User(user.id, user.name, user.address, user.email, user.strideLength, user.dailyStepGoal, user.friends)
-	userArray.push(user);
+	let currentUser = new User(user);
+	userArray.push(currentUser);
 })
 
 const currentUserData = new UserRepository(userArray);
 let randomUser = {};
-
 
 let chooseRandom = () => {
 	let randomNum = (Math.floor(Math.random() * currentUserData.data.length));
@@ -22,11 +21,12 @@ let chooseRandom = () => {
 let changeWelcome = () => {
 	chooseRandom();
 	welcomeTitle.innerText = `Welcome ${randomUser.name}!`;
+	userCardName.innerText = `Name: ${randomUser.name}`;
+	userAddress.innerText = `Address: ${randomUser.address}`;
+	userEmail.innerText = `Email: ${randomUser.email}`;
 }
 
 
 window.addEventListener('load', changeWelcome);
-//DOM -- info card displays all user's info
 
-//step goal - display how current user's step goal
-//compares to avg step goal for all users
+//step goal - display how current user's step goal compares to avg step goal for all users
