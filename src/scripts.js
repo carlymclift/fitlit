6d7b6609ca1d6ffab9aa0f518e6b4d4d90efd87a
Becca Steinbrecher <beccasteinbrecher@gmail.com>
2020-05-14 20:11:11 -0600

    Display current users today hydration

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 let welcomeTitle = document.querySelector('.welcome');
 let userCardName = document.querySelector('.name');
 let userAddress = document.querySelector('.address');
@@ -9,50 +10,57 @@ let todayH2o = document.querySelector('.today-h2o');
 let weekh2o = document.querySelector('.week-h2o');
 
 let userArray = [];
+let randomUser = {};
+let currentHydration = {};
+const userRepo = new UserRepository(userArray);
 
-
-userData.forEach(user => {
+userData.forEach(user => { // TODO: use map prototype
 	let currentUser = new User(user);
 	userArray.push(currentUser);
 })
 
-let randomUser = {};
-const currentUserData = new UserRepository(userArray);
-let currentHydration = new Hydration(randomUser, hydrationData);
-currentHydration.correctHydroData();
-
-
-let chooseRandom = () => {
-	let randomNum = (Math.floor(Math.random() * currentUserData.data.length));
-	randomUser = currentUserData.data[randomNum];
+const chooseRandom = () => {
+	const randomNum = (Math.floor(Math.random() * userRepo.data.length));
+	randomUser = userRepo.data[randomNum];
+	
+	const newHydration = new Hydration(randomUser, hydrationData)
+	
+	newHydration.correctHydroData();
+	console.log("after hydration", newHydration)
+	
+	//currentHydration = newHydration;
+	// console.log("USER", randomUser)
+	
+	updateHydration(newHydration);
 }
 
-let updateWelcome = () => {
-	userCardName.innerText = `Name: ${randomUser.name}`;
-	userAddress.innerText = `Address: ${randomUser.address}`;
-	userEmail.innerText = `Email: ${randomUser.email}`;
+// let currentHydration = new Hydration(randomUser, hydrationData);
+
+const updateWelcome = (currentUser) => {
+	userCardName.innerText = `Name: ${currentUser.name}`;
+	userAddress.innerText = `Address: ${currentUser.address}`;
+	userEmail.innerText = `Email: ${currentUser.email}`;
 }
 
-let updateHydration = () => {
-	let ouncesForDay = currentHydration.findOuncesForDay('2019/09/22');
-
+const updateHydration = (hydrationData) => {
+	let ouncesForDay = hydrationData.findOuncesForDay('2019/09/22');
+	console.log('OUNCES :', hydrationData.findOuncesForDay)
 	//avgH2o.innerText = `All user's daily average is `;
 	todayH2o.innerText = `Your water intake today is ${ouncesForDay}`;
 	weekh2o.innerText = `Your past week's water intake: `;
 }
 
-let updateSteps = () => {
+const updateSteps = () => {
 	userStepGoal.innerText = `Your Step Goal: ${randomUser.dailyStepGoal}`;
-	averageStepGoal.innerText = `Average step goal for all users: ${currentUserData.fetchAverageStepGoal()}`
+	averageStepGoal.innerText = `Average step goal for all users: ${userRepo.fetchAverageStepGoal()}`
 }
 
-let updateOnload = () => {
+const updateOnload = () => {
 	chooseRandom();
 	welcomeTitle.innerText = `Welcome ${randomUser.name}!`;
-
-	updateWelcome();
+	updateWelcome(randomUser);
 	updateSteps();
-	updateHydration();
+	// updateHydration();
 }
 
 window.addEventListener('load', updateOnload);
