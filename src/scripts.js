9af753d50847767c53920560444fcb7e1c2b3c20
Becca Steinbrecher <beccasteinbrecher@gmail.com>
2020-05-14 20:42:56 -0600

    Refactor pass

--- src/scripts.js
+++ src/scripts.js
@@ -9,58 +9,45 @@ let avgH2o = document.querySelector('.h2o-avg');
 let todayH2o = document.querySelector('.today-h2o');
 let weekh2o = document.querySelector('.week-h2o');
 
-let userArray = [];
+const userArray = userData.map((user) => new User(user));
+const userRepo = new UserRepository(userArray);
 let randomUser = {};
 let currentHydration = {};
-const userRepo = new UserRepository(userArray);
-
-userData.forEach(user => { // TODO: use map prototype
-	let currentUser = new User(user);
-	userArray.push(currentUser);
-})
 
 const chooseRandom = () => {
 	const randomNum = (Math.floor(Math.random() * userRepo.data.length));
+
 	randomUser = userRepo.data[randomNum];
-	
-	const newHydration = new Hydration(randomUser, hydrationData)
-	
-	newHydration.correctHydroData();
-	console.log("after hydration", newHydration)
-	
-	//currentHydration = newHydration;
-	// console.log("USER", randomUser)
-	
-	updateHydration(newHydration);
+	currentHydration = new Hydration(randomUser, hydrationData);
+	currentHydration.correctHydroData();	
 }
 
-// let currentHydration = new Hydration(randomUser, hydrationData);
-
 const updateWelcome = (currentUser) => {
+	welcomeTitle.innerText = `Welcome ${currentUser.name}!`;
 	userCardName.innerText = `Name: ${currentUser.name}`;
 	userAddress.innerText = `Address: ${currentUser.address}`;
 	userEmail.innerText = `Email: ${currentUser.email}`;
 }
 
-const updateHydration = (hydrationData) => {
-	let ouncesForDay = hydrationData.findOuncesForDay('2019/09/22');
-	console.log('OUNCES :', hydrationData.findOuncesForDay)
+const updateHydration = (currentHydro) => {
+	const ouncesForDay = currentHydro.findOuncesForDay('2019/09/22');
+
 	//avgH2o.innerText = `All user's daily average is `;
 	todayH2o.innerText = `Your water intake today is ${ouncesForDay}`;
-	weekh2o.innerText = `Your past week's water intake: `;
+	weekh2o.innerText = `Your past week's water intake: `; //TODO: add in list
 }
 
-const updateSteps = () => {
+const updateSteps = () => { //TODO: pass in argument like other fn's
 	userStepGoal.innerText = `Your Step Goal: ${randomUser.dailyStepGoal}`;
-	averageStepGoal.innerText = `Average step goal for all users: ${userRepo.fetchAverageStepGoal()}`
+	averageStepGoal.innerText = 
+		`Average step goal for all users: ${userRepo.fetchAverageStepGoal()}`;
 }
 
 const updateOnload = () => {
 	chooseRandom();
-	welcomeTitle.innerText = `Welcome ${randomUser.name}!`;
 	updateWelcome(randomUser);
+	updateHydration(currentHydration);
 	updateSteps();
-	// updateHydration();
 }
 
 window.addEventListener('load', updateOnload);
