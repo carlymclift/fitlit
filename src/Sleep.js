738dce227a40f1383ed68ea675cae53ad70fc3af
Carly Clift <carlymclift@gmail.com>
2020-05-16 14:07:01 -0600

    Clean up formatting

--- src/Sleep.js
+++ src/Sleep.js
@@ -1,111 +1,111 @@
 class Sleep {
     constructor(userInfo, givenSleepData) {
-        this.id = userInfo.id;
-        this.userSleepData = givenSleepData;
+      this.id = userInfo.id;
+      this.userSleepData = givenSleepData;
     }
     getUserSleepData() {
-		const currentSleepData = this.userSleepData.filter(user => {
-			return user.userID === this.id;
-		})
-		this.userSleepData = currentSleepData;
+      const currentSleepData = this.userSleepData.filter(user => {
+        return user.userID === this.id;
+      })
+      this.userSleepData = currentSleepData;
     }
-
+  
     findUserAverageSleep(dataset) {
-		let avSleep = dataset.reduce((acc, user) => {
-			acc += user.hoursSlept;
-			return acc;
-		}, 0)
-		return Math.round(avSleep / dataset.length);
+      let avSleep = dataset.reduce((acc, user) => {
+        acc += user.hoursSlept;
+        return acc;
+      }, 0)
+      return Math.round(avSleep / dataset.length);
     }
-    
+  
     findUserAverageQuality(dataset) {
-		let avSleepQuality = dataset.reduce((acc, user) => {
-			acc += user.sleepQuality;
-			return acc;
-		}, 0)
-		return Math.round(avSleepQuality / dataset.length);
+      let avSleepQuality = dataset.reduce((acc, user) => {
+        acc += user.sleepQuality;
+        return acc;
+      }, 0)
+      return Math.round(avSleepQuality / dataset.length);
     }
-
+  
     findUserSleepForDay(date) {
-		let sleepData = this.userSleepData.find(user => {
-			return user.date === date;
-		})
-		return sleepData.hoursSlept;
+      let sleepData = this.userSleepData.find(user => {
+        return user.date === date;
+      })
+      return sleepData.hoursSlept;
     }
-    
+  
     findUserSleepQualityForDay(date) {
-		let sleepData = this.userSleepData.find(user => {
-			return user.date === date;
-		})
-		return sleepData.sleepQuality;
+      let sleepData = this.userSleepData.find(user => {
+        return user.date === date;
+      })
+      return sleepData.sleepQuality;
     }
-
+  
     findUserAverageSleepForWeek(date) {
-		let currentIndex = this.userSleepData.findIndex(x => x.date === date);
-
-		let pastWeek = [];
-		for (let i = currentIndex - 6; i <= currentIndex; i++) {
-			pastWeek.push(this.userSleepData[i].hoursSlept);
-		}
-
-		return Math.round((pastWeek.reduce((a, b) => a + b)) / 7);
+      let currentIndex = this.userSleepData.findIndex(x => x.date === date);
+  
+      let pastWeek = [];
+      for (let i = currentIndex - 6; i <= currentIndex; i++) {
+        pastWeek.push(this.userSleepData[i].hoursSlept);
+      }
+  
+      return Math.round((pastWeek.reduce((a, b) => a + b)) / 7);
     }
-
+  
     findUserAverageQualityForWeek(date) {
-		let currentIndex = this.userSleepData.findIndex(x => x.date === date);
-
-		let pastWeek = [];
-		for (let i = currentIndex - 6; i <= currentIndex; i++) {
-			pastWeek.push(this.userSleepData[i].sleepQuality);
-		}
-
-		return Math.round((pastWeek.reduce((a, b) => a + b)) / 7);
+      let currentIndex = this.userSleepData.findIndex(x => x.date === date);
+  
+      let pastWeek = [];
+      for (let i = currentIndex - 6; i <= currentIndex; i++) {
+        pastWeek.push(this.userSleepData[i].sleepQuality);
+      }
+  
+      return Math.round((pastWeek.reduce((a, b) => a + b)) / 7);
     }
-    
+  
     findAverageSleep() {
-        let average = this.userSleepData.reduce((acc, user) => {
-                acc += user.hoursSlept
-                return acc;
-        }, 0)
-        return Math.round(average / this.userSleepData.length)
+      let average = this.userSleepData.reduce((acc, user) => {
+        acc += user.hoursSlept
+        return acc;
+      }, 0)
+      return Math.round(average / this.userSleepData.length)
     }
-
+  
     // findBestSleepQualityForWeek(date) {
     //     let currentIndex = this.userSleepData.findIndex(x => x.date === date);
-
-	// 	let sleepyPpl = [];
-	// 	for (let i = currentIndex - 6; i <= currentIndex; i++) {
-	// 		if(this.userSleepData[i].sleepQuality > 3) {
+  
+    // 	let sleepyPpl = [];
+    // 	for (let i = currentIndex - 6; i <= currentIndex; i++) {
+    // 		if(this.userSleepData[i].sleepQuality > 3) {
     //             sleepyPpl.push(this.userSleepData[i].userID); //Find name of user based on id, right now I think it's returning ID #
     //         }
     //     }
     //     return sleepyPpl;
     // }
-
-    findBestSleepQualityForWeek(date) {
-        let userIds = this.getUserSleepData()
-        return userIds.reduce((finalAcc, id) => {
-          let thisUsersStuff = userIds.reduce((acc, stat) => {
-            if (stat.userID === id) {
-              acc.push(stat)
-            }
-            return acc
-          }, [])
-          let index = thisUsersStuff.findIndex(stat => stat.date === date);
-          let justThisWeek = thisUsersStuff.slice(index - 6, index + 1);
-          let fakeAcc = 0;
-          let userId = 0;
-          justThisWeek.forEach(stat => {
-            fakeAcc += stat.sleepQuality / 7;
-            userId = stat.userID
-          })
-          if (fakeAcc > 3) {
-            finalAcc.push(userId)
-          }
-          return finalAcc
-        }, [])
-      }
-
+  
+    // findBestSleepQualityForWeek(date) {
+    //     // let userIds = this.getUserSleepData()
+    //     return this.userSleepData.reduce((finalAcc, id) => {
+    //       let thisUsersStuff = this.userSleepData.reduce((acc, stat) => {
+    //         if (stat.userID === id) {
+    //           acc.push(stat)
+    //         }
+    //         return acc
+    //       }, [])
+    //       let index = thisUsersStuff.findIndex(stat => stat.date === date);
+    //       let justThisWeek = thisUsersStuff.slice(index - 6, index + 1);
+    //       let fakeAcc = 0;
+    //       let userId = 0;
+    //       justThisWeek.forEach(stat => {
+    //         fakeAcc += stat.sleepQuality / 7;
+    //         userId = stat.userID
+    //       })
+    //       if (fakeAcc > 3) {
+    //         finalAcc.push(userId)
+    //       }
+    //       return finalAcc
+    //     }, [])
+    //   }
+  
     // findBestSleepQualityForWeek(date) {
     //     const weekOfUsersFn = this.findUserAverageQualityForWeek(date)
     //     const bestSlepers = [];
@@ -119,14 +119,14 @@ class Sleep {
     //     }
     //     return bestSlepers
     //   }
-
+  
     findUsersWhoSleptMost() {
-
+  
     }
-
+  
     /// Make metric of our own that we want to display
-} 
-
-if (typeof module !== 'undefined') {
-	module.exports = Sleep;
-}
+  }
+  
+  if (typeof module !== 'undefined') {
+    module.exports = Sleep;
+  }
